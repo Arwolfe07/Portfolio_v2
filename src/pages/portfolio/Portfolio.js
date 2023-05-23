b5959204fc8fcd7857cfdcba8009a01aa3c63e25
Aditya Rana <121423906+Arwolfe07@users.noreply.github.com>
2023-05-23 22:21:51 +0530

    Update Portfolio.js

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -109,7 +109,7 @@ const portfolio = [
             {
                 icon: <FiExternalLink />,
                 title: 'Preview : ',
-                desc: 'https://cryptofunk-1.onrender.com/cryptocurrencies',
+                desc: <a href='https://cryptofunk-1.onrender.com/cryptocurrencies' target="_blank">Click Here</a>,
             },
         ],
         descript: {
